d2d15b51612053b59f34ae35ab3aede75e6a94e3
Dionisio Fares <116839871+diofares@users.noreply.github.com>
2023-08-18 00:42:19 -0300

    Create main.dart
    construção da passagem entre as telas

---
+++ Semana Flutter/Controle de Estoque/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'cadastro.dart';
+
+void main() {
+  runApp( MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+         appBar: AppBar(
+          title: const Text("Meus Produtos", style: TextStyle(color: Colors.white, fontSize: 20,)),
+          backgroundColor: Colors.black,),
+          
+        body: const Center(
+          child: Text(''),
+        ),
+        
+        floatingActionButton: FloatingActionButton(onPressed: () { 
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const TelaCadastro()),
+          );
+         },
+        child: const Icon(Icons.add), backgroundColor: Colors.black,),
+      ),
+    );
+  }
+}
